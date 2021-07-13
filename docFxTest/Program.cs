6346484cfb27afe2faae7e118be07719b496d89c
Valerii Farafonov <farik.dp.2008@gmail.com>
2021-07-13 23:52:28 +0300

    docfx was added

---
+++ docFxTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace docFxTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var testClass = new TestClass();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
